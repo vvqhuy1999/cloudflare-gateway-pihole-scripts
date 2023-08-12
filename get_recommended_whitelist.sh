d65a93faaea580eb53a56edf85bd5469bf9daec2
vvqhuy1999 <43414825+vvqhuy1999@users.noreply.github.com>
2023-08-12 11:20:20 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -9,7 +9,6 @@ touch whitelist.csv
 urls=(
 https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
 https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
-https://raw.githubusercontent.com/nextdns/click-tracking-domains/main/domains
     https://raw.githubusercontent.com/im-sm/Pi-hole-Torrent-Blocklist/main/all-torrent-trackres.txt
     https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
     https://raw.githubusercontent.com/hagezi/dns-blocklists/main/whitelist.txt
